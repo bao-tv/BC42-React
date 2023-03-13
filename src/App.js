9c00ec9fa8070eeabdbb1b90eceb2131f52aadad
Viet Bao <vietbao.t@gmail.com>
2023-03-13 19:12:23 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// import Welcome from "./1_Component/Welcome";
+// import Home from "./BT-HomeLayout/Home";
+// import Shop from "./BT-ShopLayout/Shop";
+import BtReact from './React-1/BtReact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BtReact />
     </div>
   );
 }
